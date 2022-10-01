bfdce4b1843d7f29882b9cf3963e2f2141a349cc
FlyingPotato_131 <maksim.kirillov.2003@gmail.com>
2022-10-02 01:12:59 +0300

    поправил квиксорт для отрицательных чисел

--- quicksort.cpp
+++ quicksort.cpp
@@ -6,14 +6,14 @@ struct IntRange{
 };
 
 void qsort(IntRange rng){
-	if(rng.end - rng.begin > 1){
+	if(rng.end - rng.begin > 2){
 		IntRange res;
-		res.begin = static_cast<int *>(std::malloc((rng.end-rng.begin + 1)*4));
-		res.end = res.begin + (rng.end - rng.begin);
+		res.begin = static_cast<int *>(std::malloc((rng.end-rng.begin)*4));
+		res.end = res.begin + (rng.end - rng.begin) + 1;
 
 		int less = 0;
 		int grt = 0;
-		int len = rng.end - rng.begin + 1;
+		int len = rng.end - rng.begin;
 		
 		for(int i = 1; i < len; i++){
 			if(rng.begin[i] <= rng.begin[0]){
@@ -34,20 +34,20 @@ void qsort(IntRange rng){
 //		std::cout << std::endl;
 		std::free(res.begin);
 
-		qsort({&rng.begin[0], &rng.begin[less-1]});
-		qsort({&rng.begin[len - grt], &rng.begin[len - 1]});
-	}else if(rng.begin[0] > rng.end[0]){
-		rng.begin[0] = rng.begin[0] + rng.end[0];
-		rng.end[0] = rng.begin[0] - rng.end[0];
-		rng.begin[0] = rng.begin[0] - rng.end[0];
+		qsort({&rng.begin[0], &rng.begin[less]});
+		qsort({&rng.begin[len - grt], &rng.begin[len]});
+	}else if(rng.end - rng.begin == 2 && rng.begin[0] > rng.begin[1]){
+		rng.begin[0] = rng.begin[0] + rng.begin[1];
+		rng.begin[1] = rng.begin[0] - rng.begin[1];
+		rng.begin[0] = rng.begin[0] - rng.begin[1];
 	}
 }
 
 
 int main(){
-	int rng[20] = {7, 0, 2, 0, 0, 6, 6, 5, 2, 9, 5, 6, 6, 0, 7, 3, 6, 2, 6, 3};
+	int rng[20] = {-5, -3, -7, -5, 6, -3, 0, -5, 0, 2, 3, 6, 3, -9, 8, 5, -7, 5, 2, -6};
 //	int rng[6] = {0, 1, 0, 1, 1, 0};
-	qsort({&rng[0], &rng[19]});
+	qsort({&rng[0], &rng[19] + 1});
 //	qsort({&rng[0], &rng[11]});
 	for(int i = 0; i < 20; i++){
 		std::cout << rng[i] << ", ";
